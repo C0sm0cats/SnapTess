b105ddd0cb2b3e2a5ba7a47f56c3d62417971a15
C0sm0cats <jean.stephane.oton@gmail.com>
2026-09-24 22:16:00 +0200

    Allow slower GNOME 50 CI runners to finish Shell tests

--- scripts/test-shell.sh
+++ scripts/test-shell.sh
@@ -22,7 +22,7 @@ printf '[D-BUS Service]\nName=org.gnome.Shell.PerfHelper\nExec=%s --idle-timeout
 monitor_args=()
 if [[ "${SNAPTESS_TWO_MONITORS:-0}" == 1 ]]; then monitor_args=(--wrap "$PWD/scripts/two-monitors.sh"); fi
 GNOME_SHELL_BUILDDIR="$PWD/scripts" SNAPTESS_PERF_HELPER="$perf_helper" \
-XDG_DATA_HOME="$test_data_home" timeout 55s dbus-run-session -- \
+XDG_DATA_HOME="$test_data_home" timeout 90s dbus-run-session -- \
     gnome-shell-test-tool --headless --disable-animations \
     "${monitor_args[@]}" \
     --extension dist/snaptess@c0sm0cats.github.io.shell-extension.zip tests/shell.js
